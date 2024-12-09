bdd051f5703f17ae45b12e563295353def2fbe0f
IsraelChidera <israelchidera54@gmail.com>
2024-12-09 12:14:12 +0100

    application screen

--- app/dashboard/applicants/[id]/page.tsx
+++ app/dashboard/applicants/[id]/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { FaArrowLeft } from "react-icons/fa6";
+import Button from '@/app/components/elements/Button';
 
 const page = () => {
     return (
         <main className='py-[32px] px-[40px] bg-[#f9f9f9]'>
-            <div className='flex items-center  space-x-6'>
+            <div className='flex items-center mb-[24px] space-x-6'>
                 <FaArrowLeft className='text-[32px]' />
-                <h1 className='text-[28px] text-text font-bold mb-[24px]'>Applicant details</h1>
+                <h1 className='text-[28px] text-text font-bold'>Applicant details</h1>
             </div>
 
             <div className="bg-white rounded-[16px] p-[30px]  ">
@@ -40,74 +41,206 @@ const page = () => {
             <div className="bg-white rounded-[16px] p-[30px] text-[#1a1a1a] mt-[32px]">
                 <p className='text-[22px] font-semibold text-[#1a1a1a]'>Grading Form</p>
 
-                <div className='mt-[40px] grid grid-cols-2 gap-[40px]'>
-                    <div>
-                        <p className='text-[20px] font-medium mb-4'>
-                            1. State of residence <span className="text-red-400">{"(5 points)"}</span>
-                        </p>
+                <form className='mt-[40px] '>
+                    <div className="grid grid-cols-2 gap-[40px]">
+                        <div>
+                            <p className='text-[20px] font-medium mb-4'>
+                                1. State of residence <span className="text-red-400">{"(5 points)"}</span>
+                            </p>
 
-                        <input type="text" placeholder='Lagos' className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 ' disabled />
+                            <input type="text" placeholder='Lagos' className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 ' disabled />
 
-                        <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
-                            <option>5</option>
-                            <option>4</option>
-                            <option>3</option>
-                            <option>2</option>
-                            <option>1</option>
-                        </select>
+                            <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
+                                <option>5</option>
+                                <option>4</option>
+                                <option>3</option>
+                                <option>2</option>
+                                <option>1</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <p className='text-[20px] font-medium mb-4'>
+                                2. Are you born again? <span className="text-red-400">{"(10 points)"}</span>
+                            </p>
+
+                            <input type="text" placeholder='Yes' className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 ' disabled />
+
+                            <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
+                                <option>10</option>
+                                <option>9</option>
+                                <option>8</option>
+                                <option>7</option>
+                                <option>6</option>
+                                <option>5</option>
+                                <option>4</option>
+                                <option>3</option>
+                                <option>2</option>
+                                <option>1</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <p className='text-[20px] font-medium mb-4'>
+                                3. Are you filled with the Holy Spirit? <span className="text-red-400">{"(10 points)"}</span>
+                            </p>
+
+                            <input type="text" placeholder='Yes' className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 ' disabled />
+
+                            <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
+                                <option>10</option>
+                                <option>9</option>
+                                <option>8</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <p className='text-[20px] font-medium mb-4'>
+                                4. Highest Academic Qualification <span className="text-red-400">{"(5 points)"}</span>
+                            </p>
+
+                            <input type="text" placeholder='Postgraduate' className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 ' disabled />
+
+                            <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
+                                <option>5</option>
+                                <option>4</option>
+                                <option>3</option>
+                                <option>2</option>
+                                <option>1</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <p className='text-[20px] font-medium mb-4'>
+                                5. Briefly share your salvation experience <span className="text-red-400">{"(15 points)"}</span>
+                            </p>
+
+                            <textarea
+                                className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 '
+                                placeholder='Answer'
+                                rows={4}
+                                cols={4}
+                                disabled
+                            >
+                            </textarea>
+
+
+                            <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
+                                <option>15</option>
+                                <option>14</option>
+                                <option>13</option>
+                                <option>12</option>
+                                <option>11</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <p className='text-[20px] font-medium mb-4'>
+                                6. What is your calling or purpose? <span className="text-red-400">{"(15 points)"}</span>
+                            </p>
+
+                            <textarea
+                                className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 '
+                                placeholder='Answer'
+                                rows={4}
+                                cols={4}
+                                disabled
+                            >
+                            </textarea>
+
+
+                            <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
+                                <option>15</option>
+                                <option>14</option>
+                                <option>13</option>
+                                <option>12</option>
+                                <option>11</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <p className='text-[20px] font-medium mb-4'>
+                                7. Plans for Accommodation and Upkeep <span className="text-red-400">{"(10 points)"}</span>
+                            </p>
+
+                            <textarea
+                                className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 '
+                                placeholder='Answer'
+                                rows={4}
+                                cols={4}
+                                disabled
+                            >
+                            </textarea>
+
+
+                            <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
+                                <option>10</option>
+                                <option>9</option>
+                                <option>8</option>
+                                <option>7</option>
+                                <option>6</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <p className='text-[20px] font-medium mb-4'>
+                                8. Personal Statement <span className="text-red-400">{"(15 points)"}</span>
+                            </p>
+
+                            <textarea
+                                className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 '
+                                placeholder='Answer'
+                                rows={4}
+                                cols={4}
+                                disabled
+                            >
+                            </textarea>
+
+
+                            <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
+                                <option>15</option>
+                                <option>14</option>
+                                <option>13</option>
+                                <option>12</option>
+                                <option>11</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <p className='text-[20px] font-medium mb-4'>
+                                9. Reference <span className="text-red-400">{"(15 points)"}</span>
+                            </p>
+
+                            <textarea
+                                className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 '
+                                placeholder='Answer'
+                                rows={4}
+                                cols={4}
+                                disabled
+                            >
+                            </textarea>
+
+
+                            <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
+                                <option>15</option>
+                                <option>14</option>
+                                <option>13</option>
+                                <option>12</option>
+                                <option>11</option>
+                            </select>
+                        </div>
                     </div>
 
-                    <div>
-                        <p className='text-[20px] font-medium mb-4'>
-                            2. Are you born again? <span className="text-red-400">{"(10 points)"}</span>
+                    <div className='w-full mt-[48px]'>
+                        <p className='text-right text-[#1a1a1a] text-[22px] border-y border-y-[#d9d9d9] py-[24px]'>
+                            <span className="font-bold">Total:</span> 85
                         </p>
 
-                        <input type="text" placeholder='Yes' className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 ' disabled />
-
-                        <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
-                            <option>10</option>
-                            <option>9</option>
-                            <option>8</option>
-                            <option>7</option>
-                            <option>6</option>
-                            <option>5</option>
-                            <option>4</option>
-                            <option>3</option>
-                            <option>2</option>
-                            <option>1</option>
-                        </select>
+                        <Button classType="primary" className="w-full mt-[48px]">
+                            Save
+                        </Button>
                     </div>
-
-                    <div>
-                        <p className='text-[20px] font-medium mb-4'>
-                            Are you filled with the Holy Spirit? <span className="text-red-400">{"(10 points)"}</span>
-                        </p>
-
-                        <input type="text" placeholder='Yes' className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 ' disabled />
-
-                        <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
-                            <option>10</option>
-                            <option>9</option>
-                            <option>8</option>
-                        </select>
-                    </div>
-
-                    <div>
-                        <p className='text-[20px] font-medium mb-4'>
-                            Highest Academic Qualification <span className="text-red-400">{"(5 points)"}</span>
-                        </p>
-
-                        <input type="text" placeholder='Postgraduate' className='block rounded-[10px] w-full text-[17px] bg-[#EEEEEE] py-4 px-5 ' disabled />
-
-                        <select className="text-base w-full mt-4 px-5 py-4 border border-[#d9d9d9] rounded-[10px]" required>
-                            <option>5</option>
-                            <option>4</option>
-                            <option>3</option>
-                            <option>2</option>
-                            <option>1</option>
-                        </select>
-                    </div>
-                </div>
+                </form>
             </div>
         </main>
     )
